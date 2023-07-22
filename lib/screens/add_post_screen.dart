249dd4a9e3590d0b9f69d2a2c992c53e5a729445
My Name <myemail@example.com>
2023-07-22 20:36:10 +0700

    add post screen

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:instagram/utils/colors.dart";
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -10,11 +11,59 @@ class AddPostScreen extends StatefulWidget {
 class _AddPostScreenState extends State<AddPostScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: IconButton(
-        icon: const Icon(Icons.upload),
-        onPressed: () {},
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          title: const Text('Post To'),
+          centerTitle: false,
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Post',
+                style: TextStyle(
+                    color: Colors.blueAccent, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              // Navigator.pop(context);
+            },
+          ),
+        ),
+        body: Column(children: [
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    'https://images8.alphacoders.com/131/1318148.png',
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: const TextField(
+                    maxLines: 8,
+                    decoration: InputDecoration(
+                      hintText: 'Write Caption...',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 45,
+                  height: 45,
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: Image.network(
+                        'https://images8.alphacoders.com/131/1318148.png'),
+                  ),
+                ),
+                const Divider(color: Colors.white, thickness: 0.5),
+              ])
+        ]));
   }
 }
